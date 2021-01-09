9bae9b1554070db5c019a04be4857eb69276d1c1
VOSAICA <2190796788@qq.com>
2021-01-09 23:11:34 +0800

    finish chapter 1

--- PrimerFifthEdition/1/1.23.cpp
+++ PrimerFifthEdition/1/1.23.cpp
@@ -1,2 +1,30 @@
 #include <iostream>
+#include <string>
+#include "Sales_item.h"
 
+int main()
+{
+    Sales_item book1, book2;
+
+    int currVal = 0, val = 0;
+
+    if (std::cin >> book1)
+    {
+        int cnt = 1;
+        while (std::cin >> book2)
+        {
+            if (book1.isbn() == book2.isbn())
+            {
+                ++cnt;
+            }
+            else
+            {
+                book1 = book2;
+                std::cout << cnt << "\n";
+                cnt = 1;
+            }
+        }
+        std::cout << cnt << "\n";
+    }
+    return 0;
+}
