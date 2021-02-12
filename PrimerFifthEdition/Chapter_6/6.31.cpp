0f2a6a56c3de2e978c30d35f59d1fa98a30b255b
VOSAICA <2190796788@qq.com>
2021-02-12 22:50:45 +0800

    Continuing chapter 2

---
+++ PrimerFifthEdition/Chapter_6/6.31.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using std::cout;
+
+int &ng()
+{
+    int a = 100;
+    int &b = a;
+    return b; //不要返回局部变量的引用或指针
+}
+
+const int &ng2()
+{
+    const int a = 10;
+    const int &b = a;
+    return b;
+}
+
+int main()
+{
+    const int &k = ng2();
+    cout << k << ' ' << k;
+
+    return 0;
+}
