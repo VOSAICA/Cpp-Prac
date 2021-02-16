acffa40b486fea52b8965a586c7f8f63a19ae4cd
VOSAICA <2190796788@qq.com>
2021-02-16 20:28:35 +0800

    Starting chapter 7

---
+++ PrimerFifthEdition/Chapter_7/7.1.cpp
@@ -0,0 +1,31 @@
+#include "Sales_data.h"
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    Sales_data total;
+    if (cin >> total.bookNo >> total.revenue)
+    {
+        Sales_data trans;
+
+        while (cin >> trans.bookNo >> trans.revenue)
+        {
+            if (total.bookNo == trans.bookNo)
+                total.revenue += trans.revenue;
+            else
+            {
+                cout << "Not same book" << std ::endl;
+                total = trans;
+            }
+        }
+        cout << total.revenue << endl;
+    }
+    else
+        std::cerr << "No data?!" << endl;
+
+    return 0;
+}
