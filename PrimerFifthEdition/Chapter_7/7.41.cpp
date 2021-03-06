d10abd0bcee37b9ca366ece4a77fccc175b4ada2
VOSAICA <2190796788@qq.com>
2021-03-07 00:24:19 +0800

    Formatting

--- PrimerFifthEdition/Chapter_7/7.41.cpp
+++ PrimerFifthEdition/Chapter_7/7.41.cpp
@@ -9,7 +9,7 @@ Sales_data first_item(cin); // Sales_data(std::istream& is)
 
 int main()
 {
-    Sales_data next; // Sales_data() : Sales_data("Default", 0, 0)
+    Sales_data next;                  // Sales_data() : Sales_data("Default", 0, 0)
     Sales_data last("0-999-99999-9"); // Sales_data(const std::string s) : Sales_data(s, 0, 0)
 
     print(cout, next); // Default 0 0 0
