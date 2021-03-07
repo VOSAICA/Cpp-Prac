76b2c821a1e63608fe00208cb8eb69374f7c83a9
VOSAICA <2190796788@qq.com>
2021-03-07 22:25:53 +0800

    Add a comment

--- PrimerFifthEdition/Chapter_7/7.58.cpp
+++ PrimerFifthEdition/Chapter_7/7.58.cpp
@@ -7,7 +7,7 @@ class Example
 public:
     static constexpr double rate = 6.5;
     static constexpr int vecSize = 20;
-    static vector<double> vec;
+    static vector<double> vec; // 类内只能初始化整型类型的静态常量
 };
 
 constexpr double Example::rate;
