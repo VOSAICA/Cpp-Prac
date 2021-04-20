d6f01067e819e6516d785570f49fe379fd7f794e
vosaica <vosaica@qq.com>
2021-04-20 22:42:13 +0800

    Continuing chapter 12

---
+++ PrimerFifthEdition/Chapter_12/12.29.cpp
@@ -0,0 +1,37 @@
+#include "TextQuery.hpp"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::ifstream;
+using std::string;
+
+void find(QueryResult& qr) // do while麻烦死了
+{
+    do
+    {
+        string target;
+        if (cin >> target)
+        {
+            auto lines = qr.find(target);
+            for (auto i : lines)
+            {
+                cout << "LineNo: " << i << '\n';
+            }
+            cout << '\n';
+        }
+    } while (cin);
+}
+
+int main()
+{
+    auto ifs = ifstream("12.27_In.txt");
+    TextQuery tq(ifs);
+    QueryResult qr(tq);
+
+    find(qr);
+
+    return 0;
+}
