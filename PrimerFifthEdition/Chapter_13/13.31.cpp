07356f9d800200323198b9b2e89162dcc669d035
vosaica <vosaica@qq.com>
2021-05-31 11:46:18 +0800

    Revert "SpaceBeforeTraillingComments now avaliable through llvm12"
    This reverts commit e846e2e5ce9ac143344cef84c766b4a6dfe89b81.

--- PrimerFifthEdition/Chapter_13/13.31.cpp
+++ PrimerFifthEdition/Chapter_13/13.31.cpp
@@ -53,7 +53,7 @@ int main()
     HasPtr a("asb"), b("back"), c("csp");
     vector<HasPtr> vec{c, a, b};
 
-    std::sort(vec.begin(), vec.end()); //傻逼
+    std::sort(vec.begin(), vec.end());
 
     return 0;
 }
