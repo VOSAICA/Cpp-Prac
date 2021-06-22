3b325cfd0e1e9cdfd0f23f78b2c682ccd2e46617
vosaica <vosaica@qq.com>
2021-06-23 01:36:32 +0800

    simplify

--- PrimerFifthEdition/Chapter_14/14.44.cpp
+++ PrimerFifthEdition/Chapter_14/14.44.cpp
@@ -6,11 +6,11 @@
 using namespace std;
 
 // 可以用函数指针，函数对象（定义了重载调用运算符的类），lambda来初始化
-function<int(int, int)> add = [](int a, int b) { return a + b; };
-function<int(int, int)> subtract = [](int a, int b) { return a - b; };
-function<int(int, int)> multiply = [](int a, int b) { return a * b; };
-function<int(int, int)> divide = [](int a, int b) { return a / b; };
-function<int(int, int)> mod = [](int a, int b) { return a % b; };
+function<int(int, int)> add = plus<int>();
+function<int(int, int)> subtract = minus<int>();
+function<int(int, int)> multiply = multiplies<int>();
+function<int(int, int)> divide = divides<int>();
+function<int(int, int)> mod = modulus<int>();
 
 map<string, function<int(int, int)>> binops = {{"+", add}, {"-", subtract}, {"*", multiply}, {"/", divide}, {"%", mod}};
 
