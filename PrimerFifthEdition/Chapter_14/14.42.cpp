606c03613154c76435217faf8ee07672ca789123
vosaica <vosaica@qq.com>
2021-06-23 01:31:39 +0800

    format

--- PrimerFifthEdition/Chapter_14/14.42.cpp
+++ PrimerFifthEdition/Chapter_14/14.42.cpp
@@ -13,7 +13,8 @@ int main()
     std::vector<int> vi{2, 12, 312, 192};
     std::vector<std::string> vs{"pooh", "pooh", "shit", ""};
     std::cout << std::count_if(vi.begin(), vi.end(), [](const int& i) { return greater(i, 42); }) << '\n';
-    std::cout << *std::find_if(vs.begin(), vs.end(), [](const std::string& s) -> bool { return not_equal_to(s, "pooh"); }) << '\n';
+    std::cout << *std::find_if(vs.begin(), vs.end(), [](const std::string& s) -> bool { return not_equal_to(s, "pooh"); })
+              << '\n';
     std::transform(vi.begin(), vi.end(), vi.begin(), [](const int& i) { return mutiplies(i, 2); });
 
     for (const auto& i : vi)
