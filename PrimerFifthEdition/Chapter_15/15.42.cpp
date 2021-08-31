c9070688b80318f7958325fcd226d083b7dac7d5
vosaica <vosaica@qq.com>
2021-08-31 15:57:13 -0400

    Finishing chapter 15

---
+++ PrimerFifthEdition/Chapter_15/15.42.cpp
@@ -0,0 +1,303 @@
+#include <algorithm>
+#include <fstream>
+
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+
+std::string make_plural(size_t ctr, const std::string& word, const std::string& ending = "s")
+{
+    return (ctr > 1) ? word + ending : word;
+}
+
+class QueryResult;
+
+class TextQuery
+{
+public:
+    using line_no = std::vector<std::string>::size_type;
+    TextQuery(std::ifstream&);
+    QueryResult query(const std::string&) const;
+
+private:
+    std::shared_ptr<std::vector<std::string>> file;
+    std::map<std::string, std::shared_ptr<std::set<line_no>>> wm;
+};
+
+TextQuery::TextQuery(std::ifstream& is) : file(new std::vector<std::string>)
+{
+    std::string text;
+    while (std::getline(is, text))
+    {
+        for (auto comma_pos = std::find(text.begin(), text.end(), '.'), after_last_comma = text.begin();
+             after_last_comma < comma_pos;
+             comma_pos = std::find(after_last_comma, text.end(), '.'))
+        {
+            file->emplace_back(std::string(after_last_comma, comma_pos + 1));
+            if (comma_pos == text.end() - 1)
+            {
+                break;
+            }
+            after_last_comma = comma_pos + 1;
+        }
+
+        int n = static_cast<int>(file->size()) - 1;
+        std::istringstream line(text);
+        std::string word;
+        while (line >> word)
+        {
+            auto& lines = wm[word];
+            if (!lines)
+                lines.reset(new std::set<line_no>);
+            lines->insert(n);
+        }
+    }
+}
+
+class QueryResult
+{
+    friend std::ostream& print(std::ostream&, const QueryResult&);
+
+public:
+    QueryResult() = default;
+    QueryResult(std::string s,
+                const std::shared_ptr<std::set<TextQuery::line_no>>& p,
+                const std::shared_ptr<std::vector<std::string>>& f)
+        : sought(std::move(s)),
+          lines(p),
+          file(f)
+    {
+    }
+
+    std::set<TextQuery::line_no>::iterator begin()
+    {
+        return lines->begin();
+    }
+
+    std::set<TextQuery::line_no>::iterator end()
+    {
+        return lines->end();
+    }
+
+    std::shared_ptr<std::vector<std::string>> get_file()
+    {
+        return file;
+    }
+
+private:
+    std::string sought;
+    std::shared_ptr<std::set<TextQuery::line_no>> lines;
+    std::shared_ptr<std::vector<std::string>> file;
+};
+
+QueryResult TextQuery::query(const std::string& sought) const
+{
+    static std::shared_ptr<std::set<line_no>> nodata(new std::set<line_no>);
+    auto loc = wm.find(sought);
+    if (loc == wm.end())
+        return QueryResult(sought, nodata, file);
+    else
+        return QueryResult(sought, loc->second, file);
+}
+
+std::ostream& print(std::ostream& os, const QueryResult& qr)
+{
+    os << qr.sought << " occurs " << qr.lines->size() << " " << make_plural(qr.lines->size(), "time", "s") << std::endl;
+    for (auto& num : *qr.lines)
+        os << "\t(line " << num + 1 << ") "
+           << *(qr.file->begin() + static_cast<decltype(qr.lines->begin())::difference_type>(num)) << '\n';
+    return os;
+}
+
+class Query_base
+{
+    friend class Query;
+
+protected:
+    using line_no = TextQuery::line_no;
+    virtual ~Query_base() = default;
+
+private:
+    virtual QueryResult eval(const TextQuery&) const = 0;
+    virtual std::string rep() const = 0;
+};
+
+class Query
+{
+    friend Query operator~(const Query&);
+    friend Query operator|(const Query&, const Query&);
+    friend Query operator&(const Query&, const Query&);
+
+public:
+    Query(const std::string&);
+
+    QueryResult eval(const TextQuery& t) const
+    {
+        return q->eval(t);
+    }
+
+    std::string rep() const
+    {
+        return q->rep();
+    }
+
+private:
+    Query(const std::shared_ptr<Query_base>& query) : q(query)
+    {
+    }
+
+    std::shared_ptr<Query_base> q;
+};
+
+std::ostream& operator<<(std::ostream& os, const Query& query)
+{
+    return os << query.rep();
+}
+
+class WordQuery : public Query_base
+{
+    friend class Query;
+
+    WordQuery(const std::string& s) : query_word(s)
+    {
+    }
+
+    QueryResult eval(const TextQuery& t) const
+    {
+        return t.query(query_word);
+    }
+
+    std::string rep() const
+    {
+        return query_word;
+    }
+
+    std::string query_word;
+};
+
+inline Query::Query(const std::string& s) : q(new WordQuery(s))
+{
+}
+
+class NotQuery : public Query_base
+{
+    friend Query operator~(const Query&);
+
+    NotQuery(const Query& q) : query(q)
+    {
+    }
+
+    std::string rep() const
+    {
+        return "~(" + query.rep() + ");";
+    }
+
+    QueryResult eval(const TextQuery& text) const
+    {
+        auto result = query.eval(text);
+        auto ret_lines = std::make_shared<std::set<line_no>>();
+        auto beg = result.begin(), end = result.end();
+        auto sz = result.get_file()->size();
+        for (size_t n = 0; n != sz; ++n)
+        {
+            if (beg == end || *beg != n)
+            {
+                ret_lines->insert(n);
+            }
+            else if (beg != end)
+            {
+                ++beg;
+            }
+        }
+
+        return QueryResult(rep(), ret_lines, result.get_file());
+    }
+
+    Query query;
+};
+
+inline Query operator~(const Query& operand)
+{
+    return std::shared_ptr<Query_base>(new NotQuery(operand));
+}
+
+class BinaryQuery : public Query_base
+{
+protected:
+    BinaryQuery(const Query& l, const Query& r, const std::string& s) : lhs(l), rhs(r), opSym(s)
+    {
+    }
+
+    std::string rep() const
+    {
+        return '(' + lhs.rep() + ' ' + opSym + ' ' + rhs.rep() + ')';
+    }
+
+    Query lhs, rhs;    // 左侧右侧运算对象
+    std::string opSym; // 运算符的名字
+};
+
+class AndQuery : public BinaryQuery
+{
+    friend Query operator&(const Query&, const Query&);
+
+    AndQuery(const Query& left, const Query& right) : BinaryQuery(left, right, "&")
+    {
+    }
+
+    QueryResult eval(const TextQuery& text) const
+    {
+        auto left = lhs.eval(text), right = rhs.eval(text);
+        auto ret_lines = std::make_shared<std::set<line_no>>();
+        std::set_intersection(left.begin(),
+                              left.end(),
+                              right.begin(),
+                              right.end(),
+                              std::inserter(*ret_lines, ret_lines->begin()));
+
+        return QueryResult(rep(), ret_lines, left.get_file());
+    }
+};
+
+inline Query operator&(const Query& lhs, const Query& rhs)
+{
+    return std::shared_ptr<Query_base>(new AndQuery(lhs, rhs));
+}
+
+class OrQuery : public BinaryQuery
+{
+    friend Query operator|(const Query&, const Query&);
+
+    OrQuery(const Query& left, const Query& right) : BinaryQuery(left, right, "|")
+    {
+    }
+
+    QueryResult eval(const TextQuery& text) const
+    {
+        auto right = rhs.eval(text), left = lhs.eval(text);
+        auto ret_lines = std::make_shared<std::set<line_no>>(left.begin(), left.end());
+        ret_lines->insert(right.begin(), right.end());
+
+        return QueryResult(rep(), ret_lines, left.get_file());
+    }
+};
+
+inline Query operator|(const Query& lhs, const Query& rhs)
+{
+    return std::shared_ptr<Query_base>(new OrQuery(lhs, rhs));
+}
+
+int main()
+{
+    Query q = Query("is") & Query("and");
+    std::ifstream fin("Text2.txt");
+
+    print(std::cout, q.eval(TextQuery(fin)));
+
+    return 0;
+}
